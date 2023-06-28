15a8fe3c46dddde4583d9b7bfdb91e87b84ee26c
Mateusz <mateusz.jajkowicz@gmail.com>
2023-06-28 18:13:01 +0200

    streamerRoutes cleanup by controllers

--- server/routes/streamerRoutes.js
+++ server/routes/streamerRoutes.js
@@ -1,89 +1,19 @@
 import express from 'express';
-import Streamer from '../models/streamerModel.js';
+import {
+  createStreamer,
+  getAllStreamers,
+  getStreamerById,
+  updateStreamerVote,
+} from '../controllers/streamerController.js';
 
 const router = express.Router();
 
-// @desc    Create a new streamer
-// @route   POST /api/streamers
-// @access  Public
-router.post('/', async (req, res) => {
-  try {
-    const { name, platform, description } = req.body;
+router.post('/', createStreamer);
 
-    const streamer = new Streamer({
-      name,
-      platform,
-      description,
-      upvotes: 0,
-      downvotes: 0,
-    });
+router.get('/', getAllStreamers);
 
-    const savedStreamer = await streamer.save();
+router.get('/:streamerId', getStreamerById);
 
-    res.json(savedStreamer);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to create a new streamer.' });
-  }
-});
-
-// @desc    Fetch all streamers
-// @route   GET /api/streamers
-// @access  Public
-router.get('/', async (req, res) => {
-  try {
-    const streamers = await Streamer.find();
-
-    res.json(streamers);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch streamers.' });
-  }
-});
-
-// @desc    Fetch a specific streamer by ID from the database
-// @route   GET /api/streamers/:streamerId
-// @access  Public
-router.get('/:streamerId', async (req, res) => {
-  try {
-    const { streamerId } = req.params;
-
-    const streamer = await Streamer.findById(streamerId);
-
-    if (!streamer) {
-      return res.status(404).json({ error: 'Streamer not found.' });
-    }
-
-    res.json(streamer);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch the streamer.' });
-  }
-});
-
-// @desc    Update the streamer vote.
-// @route   PUT /streamers/:streamerId/vote
-// @access  Public
-router.put('/:streamerId/vote', async (req, res) => {
-  try {
-    const { streamerId } = req.params;
-    const { voteType } = req.body;
-
-    const streamer = await Streamer.findById(streamerId);
-
-    if (!streamer) {
-      return res.status(404).json({ error: 'Streamer not found.' });
-    }
-
-    if (voteType === 'upvote') {
-      streamer.upvotes++;
-    } else if (voteType === 'downvote') {
-      streamer.downvotes++;
-    }
-
-    const updatedStreamer = await streamer.save();
-
-    res.json(updatedStreamer);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to update the streamer vote.' });
-  }
-});
+router.put('/:streamerId/vote', updateStreamerVote);
 
 export default router;
